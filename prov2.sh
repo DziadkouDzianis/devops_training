b40d29177d5ac539f92933e5197b8ca75b066943
admin <DziadkouDzianis@users.noreply.github.com>
2017-02-08 09:38:14 +0300

    asd

--- prov2.sh
+++ prov2.sh
@@ -2,7 +2,7 @@ sudo yum install java-1.8.0-openjdk -y
 sudo yum install tomcat tomcat-webapps tomcat-admin-webapps -y
 #sudo systemctl restart network
 #sudo systemctl stop firewalld
-#sudo systemctl disable firewalld
+#sudo systemctl disable firewalld f
 sudo systemctl enable tomcat 
 sudo systemctl start tomcat 
 sudo sh -s
