11585a991bd0f8fa6976068c06ef741122ab4493
task3 <DziadkouDzianis@users.noreply.github.com>
2017-02-23 14:06:16 +0300

    .

--- all_prov.sh
+++
@@ -1,3 +0,0 @@
-sudo systemctl restart network
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
--- class.groovy
+++
@@ -1,8 +0,0 @@
-class GroovyClass {
-	static void main(String[] args) {
-		def mainVer = 1;
-		def minorVer = 0.01;
-		//minorVer++;
-		println(mainVer + minorVer);
-	}
-}
